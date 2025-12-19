c62caf0a96c589fcba6b1276acb06683d125e48e
Darragh <darraghdrennan5gmail.com>
2025-12-19 11:10:45 +0000

    Create basic file structure for index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Darragh");
+});
+
+const taskRoutes = require('./routes/tasks');
+app.use('/tasks', taskRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
